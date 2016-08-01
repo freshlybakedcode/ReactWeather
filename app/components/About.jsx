4f0688e7006736a06f5ad998277b366ad25ac1d2
James Brenton <freshlybakedcode@jamesbrenton.co.uk>
2016-08-01 16:01:25 +0100

    Added custom CSS and modified some styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -18,7 +18,7 @@ var {Link} = require('react-router');
 var About = () => {
   return (
     <div>
-      <h1 className="text-center">Examples</h1>
+      <h1 className="text-center page-title">Examples</h1>
       <h2 className="text-center">A weather app built using React</h2><br />
       <p>The source code for this site is hosted <Link to='https://github.com/freshlybakedcode/ReactWeather'>on github</Link></p>
       <p>The weather API is courtesy of <Link to='http://openweathermap.org/'>OpenWeatherMap</Link></p>
