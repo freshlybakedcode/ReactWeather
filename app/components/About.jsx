bcf705ecf07035a22cce4fb4471f9aa3dcccef00
James Brenton <freshlybakedcode@jamesbrenton.co.uk>
2016-08-02 15:56:11 +0100

    Fixed hyperlink issues on the about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -20,8 +20,8 @@ var About = () => {
     <div>
       <h1 className="text-center page-title">Examples</h1>
       <h2 className="text-center">A weather app built using React</h2><br />
-      <p>The source code for this site is hosted <Link to='https://github.com/freshlybakedcode/ReactWeather'>on github</Link></p>
-      <p>The weather API is courtesy of <Link to='http://openweathermap.org/'>OpenWeatherMap</Link></p>
+      <p>The source code for this site is hosted <a href='https://github.com/freshlybakedcode/ReactWeather'>on github</a></p>
+      <p>The weather API is courtesy of <a href='http://openweathermap.org/'>OpenWeatherMap</a></p>
     </div>
   )
 };
